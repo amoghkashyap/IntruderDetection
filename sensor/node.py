70280e852c61abaeaffe4b38f3877b40252eae40
amoghkashyap <amogh.h.kashyap@gmail.com>
2018-12-09 12:45:11 +0530

    refactorng

--- sensor/node.py
+++ sensor/node.py
@@ -1,24 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 import os
+import requests
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 GPIO.setup(17,GPIO.IN)
 
+def captureImage():
+    os.system('raspistill -o /home/pi/Documents/intruder.jpg')
 
-while True:
-    input=GPIO.input(17)
-    if input==1:
-        print("Intruder Detected")
-        os.system('raspistill -o /home/pi/Documents/intruder.jpg')
-        print("capture successful")
-        print ("LED on")
-        GPIO.output(18,GPIO.HIGH)
-        time.sleep(2)
-        print ("LED off")
-        GPIO.output(18,GPIO.LOW)
-        time.sleep(1)
-    else:
-        print("No Intruder")
-        time.sleep(1)
+def ledTrigger():
+    print ("LED on")
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(2)
+    print ("LED off")
+    GPIO.output(18,GPIO.LOW)
+
+def serverCalls(url,data):
+    response = requests.post(url,json=data)
+    print (response)
+
+if __name__ == "__main__":
+    while True:
+        input=GPIO.input(17)
+        if input==1:
+            print("Intruder Detected")
+            captureImage()
+            print("capture successful")
+            serverCalls("http://localhost:50010/register","amogh")
+            ledTrigger()
+        else:
+            print("No Intruder")
+            time.sleep(1)
